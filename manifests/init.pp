8edcccee970b840b307df42fb27c922705d6d3eb
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 16:18:40 +0200

    initial module release

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,28 @@
 
 
 # modules_dir { "syslog": }
-class syslog {}
+class syslog {
+    case $operatingsystem {
+        centos: { include syslog::centos }
+        default: { include syslog::base }
+    }
+}
+
+class syslog::base {
+    package{syslog:
+        ensure => present,
+    }
+
+    service{syslog:
+        ensure => running,
+        hasstatus => true,
+        require => Package[syslog],
+    }
+}
+
+class syslog::centos inherits {
+    Package[syslog]{
+        name => 'sysklogd',
+    }
+    include syslog::pbpfixes
+}
---
+++ manifests/pbpfixes.pp
@@ -0,0 +1,24 @@
+#######################################
+# syslog module - pbpfixes.pp
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+# puzzle best practices for syslog
+#######################################
+
+# ToDo: This should be managed all in a own config file
+class syslog::pbpfixes {
+    exec{syslog_firewall:
+        command => 'echo "kern.warn  /var/log/firewall" >> /etc/syslog.conf',
+        unless => 'grep -q "/var/log/firewall" /etc/syslog.conf',
+        require => Package[syslog],
+        notify => Service[syslog],
+    }
+
+    exec{console_logging:
+        command => 'sed -i "s@\(KLOGD_OPTIONS=\)\"\(.*\)\"@\1\"\2 -c 4\"@g" etc/sysconfig/syslog',
+        unless => 'grep -q "KLOGD_OPTIONS.*-c 4" /etc/sysconfig/syslog',
+        require => Package[syslog],
+        notify => Service[syslog],
+    }
+ 
+}
