106e2929a0e90d5d7d9375413a2b9232992472f7
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 14:17:03 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# syslog module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "syslog": }
+class syslog {}
